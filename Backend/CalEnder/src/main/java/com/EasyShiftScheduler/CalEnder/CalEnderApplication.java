472030748da1c7beb89a9578f94b0fe872236a97
Jason <95380767+Jaiwin42@users.noreply.github.com>
2026-03-25 18:57:51 -0500

    Backend Sprint Things

---
+++ Backend/CalEnder/src/main/java/com/EasyShiftScheduler/CalEnder/CalEnderApplication.java
@@ -0,0 +1,13 @@
+package com.EasyShiftScheduler.CalEnder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalEnderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalEnderApplication.class, args);
+	}
+
+}
