0ceff2b43f2690c49a3260b0d4d4e64dc646cc42
a <a@a>
2026-03-25 19:08:21 -0500

    Folder Struct #2

--- Backend/CalEnder/src/main/java/com/EasyShiftScheduler/CalEnder/Services/CalEnderApplication.java
+++ Backend/CalEnder/src/main/java/com/EasyShiftScheduler/CalEnder/Services/CalEnderApplication.java
@@ -1,4 +1,4 @@
-package com.EasyShiftScheduler.CalEnder;
+CalEnderApplication.javapackage com.EasyShiftScheduler.CalEnder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
