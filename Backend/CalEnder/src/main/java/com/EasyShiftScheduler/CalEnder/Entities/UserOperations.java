f5a63e5e38449abda4adc746ea9fff0c5c4685ea
Jason <95380767+Jaiwin42@users.noreply.github.com>
2026-04-01 19:18:25 -0500

    User Changes

---
+++ Backend/CalEnder/src/main/java/com/EasyShiftScheduler/CalEnder/Entities/UserOperations.java
@@ -0,0 +1,12 @@
+package com.EasyShiftScheduler.CalEnder.Entities;
+
+public class UserOperations {
+
+    public void login(String username, String password){
+        //
+    }
+
+    public void resetPassword(String username){
+        //
+    }
+}
