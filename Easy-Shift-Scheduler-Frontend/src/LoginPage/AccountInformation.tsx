d7ab6510f199d2ee514fb5bb36222805a3ac97e0
Nick <a@a>
2026-04-01 22:18:27 -0500

    - Added Color Theme - Adjusted Login Section sizing - Added Reset Password and Create New Account <a> tags

---
+++ Easy-Shift-Scheduler-Frontend/src/LoginPage/AccountInformation.tsx
@@ -0,0 +1,14 @@
+import './CSS/LoginPage.css'
+
+export default function AccountInformation() {
+    return (
+        <section className="account-information">
+            <a>
+                Reset Password
+            </a>
+            <a>
+                Create New Account
+            </a>
+        </section>
+    )
+}
--- Easy-Shift-Scheduler-Frontend/src/LoginPage/LoginPage.tsx
+++ Easy-Shift-Scheduler-Frontend/src/LoginPage/LoginPage.tsx
@@ -1,14 +1,14 @@
-import '../CSS/LoginPage.css'
+import './CSS/LoginPage.css'
 import InputSection from "./InputSection.tsx"
+import AccountInformation from "./AccountInformation.tsx"
 
 export default function LoginPage() {
     return (
         <section className="login-section">
-            <h1 className="login-section-title">
-                Login
-            </h1>
-            <InputSection title={"Username"} inputType={"text"} placeholder={"Username"}/>
+            <InputSection title={"Email"} inputType={"text"} placeholder={"Email"}/>
             <InputSection title={"Password"} inputType={"password"} placeholder={"Password"}/>
+            <input className={"login-button"} type={"submit"} value={"Login"} />
+            <AccountInformation />
         </section>
     )
 }
