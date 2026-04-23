cb43c34efe54a7a98f1185dc1ceb521f8572966b
Jason <95380767+Jaiwin42@users.noreply.github.com>
2026-04-22 19:19:01 -0500

    Create Group Use Case

--- Backend/CalEnder/src/main/java/com/EasyShiftScheduler/CalEnder/Controllers/GroupController.java
+++ Backend/CalEnder/src/main/java/com/EasyShiftScheduler/CalEnder/Controllers/GroupController.java
@@ -1,10 +1,16 @@
 package com.EasyShiftScheduler.CalEnder.Controllers;
 
-import com.EasyShiftScheduler.CalEnder.Services.GroupService;
+import java.lang.classfile.Label;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.EasyShiftScheduler.CalEnder.Entities.Group;
+import com.EasyShiftScheduler.CalEnder.Services.GroupService;
 
 @RestController
 @RequestMapping("/api/group")
@@ -19,5 +25,10 @@ public class GroupController {
     @DeleteMapping("/remove-user-from-group")
     public String removeEmployeeFromGroup(@RequestParam("groupID") long groupID, @RequestParam("employeeID") long employeeID, @RequestParam("employerID") long employerID) {
         return groupService.removeEmployee(groupID, employeeID, employerID);
+    }
+
+    @PostMapping("/create-group")
+    public Group createGroup(@RequestParam("userIDs") List<Long> userIDs){
+        return groupService.createGroup(userIDs);
     }
 }
