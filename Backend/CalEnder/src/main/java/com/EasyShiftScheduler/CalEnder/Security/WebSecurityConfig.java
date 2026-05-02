f534ce1d907d32dd95fedb734116703a53f1517e
Jake <jaketc05@gmail.com>
2026-05-02 16:51:34 -0500

    yaml changes

--- Backend/CalEnder/src/main/java/com/EasyShiftScheduler/CalEnder/Security/WebSecurityConfig.java
+++ Backend/CalEnder/src/main/java/com/EasyShiftScheduler/CalEnder/Security/WebSecurityConfig.java
@@ -58,8 +58,8 @@ public class WebSecurityConfig {
                                 .requestMatchers("/api/schedule/{userID}/swap-shift").hasAnyAuthority("EMPLOYEE", "EMPLOYER")
                                 .requestMatchers("/api/schedule/{userID}/pickup-shift").hasAnyAuthority("EMPLOYEE", "EMPLOYER")
                                 .requestMatchers("/api/auth/**").permitAll() // Use 'requestMatchers' instead of 'antMatchers'
-                                .requestMatchers("/api/user/{userID}/avail-schedule/update").hasAnyAuthority("EMPLOYEE", "EMPLOYER")
-                                .requestMatchers("/api/user/{userID}/avail-schedule").hasAnyAuthority("EMPLOYEE", "EMPLOYER")
+                                .requestMatchers("/api/schedule/{userID}/avail-schedule/update").hasAnyAuthority("EMPLOYEE", "EMPLOYER")
+                                .requestMatchers("/api/schedule/{userID}/avail-schedule").hasAnyAuthority("EMPLOYEE", "EMPLOYER")
                                 .requestMatchers("/api/time-off-request/{id}/request-time-off").hasAnyAuthority("EMPLOYEE", "EMPLOYER")
                                 .requestMatchers("/api/time-off-request/view-time-off-requests").hasAuthority("EMPLOYER")
                                 .requestMatchers("/api/time-off-request/{requestID}/approve").hasAuthority("EMPLOYER")
