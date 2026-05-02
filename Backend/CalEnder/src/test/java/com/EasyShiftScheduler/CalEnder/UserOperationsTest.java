7713665fa72d59330326bda6e1194b5ca999d9e2
Jake <jaketc05@gmail.com>
2026-05-02 14:26:48 -0500

    Coolio

--- Backend/CalEnder/src/test/java/com/EasyShiftScheduler/CalEnder/UserOperationsTest.java
+++ Backend/CalEnder/src/test/java/com/EasyShiftScheduler/CalEnder/UserOperationsTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class UserOperationsTest {
 	private UserOperations userOperations;
 
-	@BeforeTestClass
+	@BeforeEach
 	public void setUp() {
 		userOperations = new UserOperations();
 	}
