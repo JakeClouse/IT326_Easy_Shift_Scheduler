d93f4cf8f90090e2a17aaa0e3e228c37946a941c
Jake <jaketc05@gmail.com>
2026-05-02 17:36:54 -0500

    Welcome back to slop live

--- Backend/CalEnder/src/main/java/com/EasyShiftScheduler/CalEnder/Services/UserTimeCardService.java
+++ Backend/CalEnder/src/main/java/com/EasyShiftScheduler/CalEnder/Services/UserTimeCardService.java
@@ -3,6 +3,7 @@ package com.EasyShiftScheduler.CalEnder.Services;
 import com.EasyShiftScheduler.CalEnder.Entities.Punch;
 import com.EasyShiftScheduler.CalEnder.Entities.User;
 import com.EasyShiftScheduler.CalEnder.Entities.UserTimecard;
+import com.EasyShiftScheduler.CalEnder.Repositories.PunchRepository;
 import com.EasyShiftScheduler.CalEnder.Repositories.UserRepository;
 import com.EasyShiftScheduler.CalEnder.Repositories.UserTimecardRepository;
 import org.springframework.stereotype.Service;
@@ -16,9 +17,11 @@ import java.util.Optional;
 @Service
 public class UserTimeCardService {
     private final UserRepository userRepository;
+    private final PunchRepository punchRepository;
 
-    public UserTimeCardService(UserRepository userRepository, UserTimecardRepository timecardRepository) {
+    public UserTimeCardService(UserRepository userRepository, UserTimecardRepository timecardRepository, PunchRepository punchRepository) {
         this.userRepository = userRepository;
+        this.punchRepository = punchRepository;
     }
 
     public String setWorkedHours(long userID, int workedHours) {
@@ -49,7 +52,9 @@ public class UserTimeCardService {
             Punch punchIn = new Punch();
             punchIn.setPunch_time(time);
             punchIn.setReason("Clock-in");
-            punchList.add(punchIn);
+            punchIn.setUser_timecard(timecard);
+            Punch savedPunch = punchRepository.save(punchIn);
+            punchList.add(savedPunch);
             timecard.setPunch_times(punchList);
             gotUser.setUser_timecard(timecard);
             userRepository.save(gotUser);
@@ -73,10 +78,13 @@ public class UserTimeCardService {
             Punch punchOut = new Punch();
             punchOut.setPunch_time(time);
             punchOut.setReason(reason);
-            Duration elapsed = Duration.between(punchOut.getPunch_time(), punchList.get(punchList.size() - 1).getPunch_time());
-            punchList.add(punchOut);
+            punchOut.setUser_timecard(timecard);
+            Punch savedPunch = punchRepository.save(punchOut);
+            Punch lastClockIn = punchList.get(punchList.size() - 1);
+            Duration elapsed = Duration.between(lastClockIn.getPunch_time(), time);
+            punchList.add(savedPunch);
             timecard.setPunch_times(punchList);
-            timecard.setWorked_hours(elapsed.toMinutes()/60.0);
+            timecard.setWorked_hours((int)(elapsed.toMinutes()/60.0));
             gotUser.setUser_timecard(timecard);
             userRepository.save(gotUser);
         } else {
